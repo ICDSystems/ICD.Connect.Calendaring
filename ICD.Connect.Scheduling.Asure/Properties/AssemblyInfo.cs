725960d84c23ab37d1b8a4fa7a4fc17857cfd2d8
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-24 10:27:50 -0400

    chore: Updating changelog and major version number

--- ICD.Connect.Scheduling.Asure/Properties/AssemblyInfo.cs
+++ ICD.Connect.Scheduling.Asure/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Scheduling.Asure")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
