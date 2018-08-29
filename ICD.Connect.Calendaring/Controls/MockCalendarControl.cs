f9576b0299882bd29d988c5d7022caf780289c8c
Rashod Davis <rashod.davis@wedoresi.com>
2018-08-29 16:03:12 -0400

    chore: Updated MockCalendarControl

--- ICD.Connect.Calendaring/Controls/MockCalendarControl.cs
+++ ICD.Connect.Calendaring/Controls/MockCalendarControl.cs
@@ -11,36 +11,30 @@ namespace ICD.Connect.Calendaring.Controls
 	{
 
 		private readonly List<MockBooking> m_BookingList;
+		private readonly DateTime m_DefaultMeetingTime;
 
 		public MockCalendarControl(MockCalendarDevice parent, int id) : base(parent, id)
 		{
+			DateTime timeNow = IcdEnvironment.GetLocalTime();
 			m_BookingList = new List<MockBooking>();
+			m_DefaultMeetingTime = new DateTime(timeNow.Year, timeNow.Month, timeNow.Day, timeNow.Hour, 0, 0);
 		}
 
 		public override event EventHandler OnBookingsChanged;
 
 		public override void Refresh()
 		{
-			m_BookingList.Add(new MockBooking("Meeting1", "Organizer1@email.biz", "The Organizer1", new DateTime(2018, 8, 27, 12, 30, 0), new DateTime(2018, 8, 27, 13, 0, 0)));
-			m_BookingList.Add(new MockBooking("Meeting2", "Organizer2@email.biz", "The Organizer2", new DateTime(2018, 8, 27, 11, 30, 0), new DateTime(2018, 8, 27, 12, 0, 0)));
-			m_BookingList.Add(new MockBooking("Meeting3", "Organizer3@email.biz", "The Organizer3", new DateTime(2018, 8, 27, 10, 30, 0), new DateTime(2018, 8, 27, 11, 0, 0)));
-			m_BookingList.Add(new MockBooking("Meeting4", "Organizer4@email.biz", "The Organizer4", new DateTime(2018, 8, 27, 10, 0, 0), new DateTime(2018, 8, 27, 10, 30, 0)));
-			m_BookingList.Add(new MockBooking("Meeting5", "Organizer5@email.biz", "The Organizer5", new DateTime(2018, 8, 27, 13, 30, 0), new DateTime(2018, 8, 27, 14, 0, 0)));
+			m_BookingList.Clear();
 
-			foreach (var booking in m_BookingList)
-			{
-				IcdConsole.PrintLine(booking.ToString());
-			}
+			m_BookingList.Add(new MockBooking("Meeting1", "Organizer1@email.biz", "The Organizer1", m_DefaultMeetingTime.AddHours(-3), m_DefaultMeetingTime.AddHours(-3).AddMinutes(30)));
+			m_BookingList.Add(new MockBooking("Meeting2", "Organizer2@email.biz", "The Organizer2", m_DefaultMeetingTime.AddHours(-2).AddMinutes(30), m_DefaultMeetingTime.AddHours(-1)));
+			m_BookingList.Add(new MockBooking("Meeting3", "Organizer3@email.biz", "The Organizer3", m_DefaultMeetingTime.AddHours(-1), m_DefaultMeetingTime.AddHours(-1).AddMinutes(30)));
+			m_BookingList.Add(new MockBooking("Meeting4", "Organizer4@email.biz", "The Organizer4", m_DefaultMeetingTime, m_DefaultMeetingTime.AddMinutes(30)));
+			m_BookingList.Add(new MockBooking("Meeting5", "Organizer5@email.biz", "The Organizer5", m_DefaultMeetingTime.AddHours(1), m_DefaultMeetingTime.AddMinutes(30)));
 		}
 
 		public override IEnumerable<IBooking> GetBookings()
 		{
-			m_BookingList.Add(new MockBooking("Meeting1", "Organizer1@email.biz", "The Organizer1", new DateTime(2018, 8, 12, 12, 30, 0), new DateTime(2018, 8, 12, 13, 0, 0)));
-			m_BookingList.Add(new MockBooking("Meeting2", "Organizer2@email.biz", "The Organizer2", new DateTime(2018, 8, 12, 11, 30, 0), new DateTime(2018, 8, 12, 12, 0, 0)));
-			m_BookingList.Add(new MockBooking("Meeting3", "Organizer3@email.biz", "The Organizer3", new DateTime(2018, 8, 12, 10, 30, 0), new DateTime(2018, 8, 12, 11, 0, 0)));
-			m_BookingList.Add(new MockBooking("Meeting4", "Organizer4@email.biz", "The Organizer4", new DateTime(2018, 8, 12, 10, 0, 0), new DateTime(2018, 8, 12, 10, 30, 0)));
-			m_BookingList.Add(new MockBooking("Meeting5", "Organizer5@email.biz", "The Organizer5", new DateTime(2018, 8, 12, 13, 30, 0), new DateTime(2018, 8, 12, 14, 0, 0)));
-
 			return m_BookingList;
 		}
 	}
