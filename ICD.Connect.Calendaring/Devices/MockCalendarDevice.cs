86b0c106cac112947010577aaa302b981df3da1a
Chris Cameron <chris.cameron@wedoresi.com>
2018-09-24 13:35:46 -0400

    refactor: Tidying

--- ICD.Connect.Calendaring/Devices/MockCalendarDevice.cs
+++ ICD.Connect.Calendaring/Devices/MockCalendarDevice.cs
@@ -3,14 +3,21 @@ using ICD.Connect.Devices;
 
 namespace ICD.Connect.Calendaring.Devices
 {
-	public class MockCalendarDevice : AbstractDevice<MockCalendarDeviceSettings>
+	public sealed class MockCalendarDevice : AbstractDevice<MockCalendarDeviceSettings>
 	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
 		public MockCalendarDevice()
 		{
 			MockCalendarControl control = new MockCalendarControl(this, 1);
 			Controls.Add(control);
 		}
 
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
 		protected override bool GetIsOnlineStatus()
 		{
 			return true;
--- ICD.Connect.Calendaring/Devices/MockCalendarDeviceSettings.cs
+++ ICD.Connect.Calendaring/Devices/MockCalendarDeviceSettings.cs
@@ -4,8 +4,7 @@ using ICD.Connect.Settings.Attributes;
 namespace ICD.Connect.Calendaring.Devices
 {
 	[KrangSettings("MockCalendarDevice", typeof(MockCalendarDevice))]
-	public class MockCalendarDeviceSettings : AbstractDeviceSettings
+	public sealed class MockCalendarDeviceSettings : AbstractDeviceSettings
 	{
-		
 	}
 }
