38707ebbea664ff6a5103b079386dd6b9384c69f
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-08 09:29:51 -0400

    feat: Adding extension method for converting booking protocol to a meeting type

--- ICD.Connect.Calendaring/BookingProtocolInfo.cs
+++ ICD.Connect.Calendaring/BookingProtocolInfo.cs
@@ -1,4 +1,7 @@
-﻿namespace ICD.Connect.Calendaring
+﻿using System;
+using ICD.Connect.Calendaring.Booking;
+
+namespace ICD.Connect.Calendaring
 {
 	public sealed class BookingProtocolInfo
     {
@@ -16,5 +19,32 @@
 		Pstn = 1,
 		Sip = 2,
 		Zoom = 3
+	}
+
+	public static class BookingProtocolExtensions
+	{
+		/// <summary>
+		/// Converts the booking protocol to the best possible meeting type.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		public static eMeetingType ToMeetingType(this eBookingProtocol extends)
+		{
+			switch (extends)
+			{
+				case eBookingProtocol.Zoom:
+				case eBookingProtocol.Sip:
+					return eMeetingType.VideoConference;
+
+				case eBookingProtocol.Pstn:
+					return eMeetingType.AudioConference;
+
+				case eBookingProtocol.None:
+					return eMeetingType.Presentation;
+
+				default:
+					throw new ArgumentOutOfRangeException("extends");
+			}
+		}
 	}
 }
