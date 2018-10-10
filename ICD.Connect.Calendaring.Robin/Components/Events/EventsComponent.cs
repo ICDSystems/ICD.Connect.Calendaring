2c3ce7b92f263d2761f59f6b640cf4de54a07846
Rashod Davis <rashod.davis@wedoresi.com>
2018-10-10 07:11:49 -0700

    feat: Gets reservations based on local timezones and increased the number of meeting retrieved

--- ICD.Connect.Calendaring.Robin/Components/Events/EventsComponent.cs
+++ ICD.Connect.Calendaring.Robin/Components/Events/EventsComponent.cs
@@ -82,9 +82,9 @@ namespace ICD.Connect.Calendaring.Robin.Components.Events
 		/// <param name="startTime"></param>
 		/// <param name="endTime"></param>
 		/// <returns></returns>
-		private Event[] GetReservations(string resourceId, DateTime startTime, DateTime endTime)
+		private Event[] GetReservations(string resourceId, DateTimeOffset startTime, DateTimeOffset endTime)
 		{
-			string uri = string.Format("spaces/{0}/events?after={1:yyyy-MM-ddTHH:mm:ssZ}&before={2:yyyy-MM-ddTHH:mm:ssZ}", resourceId, startTime, endTime);
+			string uri = string.Format("spaces/{0}/events?after={1:yyyy-MM-ddTHH:mm:ss zzz}&before={2:yyyy-MM-ddTHH:mm:ss zzz}&per_page=300", resourceId, startTime, endTime);
 
 			string data = Parent.Request(uri);
 
