5fb6b5c0b68e7f7a1b198c31137b2afab0bc2210
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-18 12:41:13 -0400

    chore: Updating changelog, incrementing major version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [5.0.0] - 2018-10-18
  - Added Robin Calender device
  - Added booking number parsing
  - Added number parsing to Robin Device
--- ICD.Connect.Calendaring.Robin/Properties/AssemblyInfo.cs
+++ ICD.Connect.Calendaring.Robin/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Calendaring.Robin")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Calendaring.Robin")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
 
