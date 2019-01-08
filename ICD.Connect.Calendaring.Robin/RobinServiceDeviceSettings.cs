3def19f496556702511db28f24cf4c2b6de4c530
Chris Cameron <chris.cameron@wedoresi.com>
2019-01-08 11:26:26 -0500

    fix: Applying default robin configuration after parsing xml

--- ICD.Connect.Calendaring.Robin/RobinServiceDeviceSettings.cs
+++ ICD.Connect.Calendaring.Robin/RobinServiceDeviceSettings.cs
@@ -91,7 +91,6 @@ namespace ICD.Connect.Calendaring.Robin
 		public RobinServiceDeviceSettings()
 		{
 			m_UriProperties = new UriProperties();
-			m_UriProperties.SetUriFromAddress("https://api.robinpowered.com/v1.0/");
 		}
 
 		/// <summary>
@@ -124,6 +123,13 @@ namespace ICD.Connect.Calendaring.Robin
 			CalendarParsingPath = XmlUtils.TryReadChildElementContentAsString(xml, CALENDARPARSING_ELEMENT);
 
 			m_UriProperties.ParseXml(xml);
+
+			UpdateUriDefaults();
+		}
+
+		private void UpdateUriDefaults()
+		{
+			m_UriProperties.ApplyDefaultValuesFromAddress("https://api.robinpowered.com/v1.0/");
 		}
 	}
 }
