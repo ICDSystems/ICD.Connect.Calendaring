7e930ce750f8bd1564d373086aa1bc6a3eccb1a6
Chris Cameron <chris.cameron@wedoresi.com>
2019-06-19 13:59:33 -0400

    feat: Initial commit of Exchange project and class stubs

---
+++ ICD.Connect.Calendaring.Exchange/ExchangeServiceDeviceSettings.cs
@@ -0,0 +1,126 @@
+﻿using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices;
+using ICD.Connect.Protocol.Network.Ports.Web;
+using ICD.Connect.Protocol.Network.Settings;
+using ICD.Connect.Settings.Attributes;
+using ICD.Connect.Settings.Attributes.SettingsProperties;
+
+namespace ICD.Connect.Calendaring.Exchange
+{
+	[KrangSettings("RobinServiceDevice", typeof(ExchangeServiceDevice))]
+	public sealed class ExchangeServiceDeviceSettings : AbstractDeviceSettings, IUriSettings
+	{
+		private const string PORT_ELEMENT = "Port";
+		private const string TOKEN_ELEMENT = "Token";
+
+		private readonly UriProperties m_UriProperties;
+
+		#region Properties
+
+		/// <summary>
+		/// The port id.
+		/// </summary>
+		[OriginatorIdSettingsProperty(typeof(IWebPort))]
+		public int? Port { get; set; }
+
+		public string Token { get; set; }
+
+		#endregion
+
+		#region URI
+
+		/// <summary>
+		/// Gets/sets the configurable URI username.
+		/// </summary>
+		public string UriUsername { get { return m_UriProperties.UriUsername; } set { m_UriProperties.UriUsername = value; } }
+
+		/// <summary>
+		/// Gets/sets the configurable URI password.
+		/// </summary>
+		public string UriPassword { get { return m_UriProperties.UriPassword; } set { m_UriProperties.UriPassword = value; } }
+
+		/// <summary>
+		/// Gets/sets the configurable URI host.
+		/// </summary>
+		public string UriHost { get { return m_UriProperties.UriHost; } set { m_UriProperties.UriHost = value; } }
+
+		/// <summary>
+		/// Gets/sets the configurable URI port.
+		/// </summary>
+		public ushort? UriPort { get { return m_UriProperties.UriPort; } set { m_UriProperties.UriPort = value; } }
+
+		/// <summary>
+		/// Gets/sets the configurable URI scheme.
+		/// </summary>
+		public string UriScheme { get { return m_UriProperties.UriScheme; } set { m_UriProperties.UriScheme = value; } }
+
+		/// <summary>
+		/// Gets/sets the configurable URI path.
+		/// </summary>
+		public string UriPath { get { return m_UriProperties.UriPath; } set { m_UriProperties.UriPath = value; } }
+
+		/// <summary>
+		/// Gets/sets the configurable URI query.
+		/// </summary>
+		public string UriQuery { get { return m_UriProperties.UriQuery; } set { m_UriProperties.UriQuery = value; } }
+
+		/// <summary>
+		/// Gets/sets the configurable URI fragment.
+		/// </summary>
+		public string UriFragment { get { return m_UriProperties.UriFragment; } set { m_UriProperties.UriFragment = value; } }
+
+		/// <summary>
+		/// Clears the configured values.
+		/// </summary>
+		void IUriProperties.Clear()
+		{
+			m_UriProperties.Clear();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public ExchangeServiceDeviceSettings()
+		{
+			m_UriProperties = new UriProperties();
+			UpdateUriDefaults();
+		}
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(PORT_ELEMENT, IcdXmlConvert.ToString(Port));
+			writer.WriteElementString(TOKEN_ELEMENT, Token);
+
+			m_UriProperties.WriteElements(writer);
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			Port = XmlUtils.TryReadChildElementContentAsInt(xml, PORT_ELEMENT);
+			Token = XmlUtils.TryReadChildElementContentAsString(xml, TOKEN_ELEMENT);
+
+			m_UriProperties.ParseXml(xml);
+
+			UpdateUriDefaults();
+		}
+
+		private void UpdateUriDefaults()
+		{
+			//m_UriProperties.ApplyDefaultValuesFromAddress("https://api.robinpowered.com/v1.0/");
+		}
+	}
+}
---
+++ ICD.Connect.Calendaring.Exchange/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Calendaring.Exchange")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ICD.Connect.Calendaring.Exchange")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
