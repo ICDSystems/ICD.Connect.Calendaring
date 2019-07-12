8a8d25dba01e30940f7e43b7f18843b4a79a40c7
Laura Gomez <laura.gomez@profound-tech.com>
2019-07-12 12:26:10 -0400

    feat: Parsing the iCalendar including booing, attendee, event and calendar.

---
+++ ICD.Connect.Calendaring/Devices/iCalendar/Parser/iCalendarBooking.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.Calendaring.Booking;
+using ICD.Connect.Calendaring.Devices.iCalendar.Parser;
+using ICD.Connect.Conferencing.DialContexts;
+
+namespace ICD.Connect.Calendaring.iCalendar.Controls.Calendar
+{
+// ReSharper disable InconsistentNaming
+	public sealed class iCalendarBooking : AbstractBooking
+// ReSharper restore InconsistentNaming
+	{
+		private readonly iCalendarEvent m_Event;
+		private readonly IList<IDialContext> m_BookingNumbers;
+
+		public iCalendarEvent Event { get { return m_Event; } }
+
+		public override string MeetingName
+		{
+			get { return m_Event.Summary; }
+		}
+
+		public override string OrganizerName
+		{
+			get { return m_Event.Organizer == null ? null : m_Event.Organizer.Cn; }
+		}
+
+		public override string OrganizerEmail
+		{
+			get
+			{
+				if (m_Event.Organizer == null)
+					return null;
+
+				return m_Event.Organizer.Mailto ?? m_Event.Organizer.Cn;
+			}
+		}
+
+		public override DateTime StartTime
+		{
+			get { return m_Event.DtStart.ToLocalTime(); }
+		}
+
+		public override DateTime EndTime
+		{
+			get { return m_Event.DtEnd.ToLocalTime(); }
+		}
+
+		public override bool IsPrivate
+		{
+			get { return m_Event.Class != "PUBLIC"; }  
+		}
+
+		public override IEnumerable<IDialContext> GetBookingNumbers()
+		{
+			return m_BookingNumbers.ToArray(m_BookingNumbers.Count);
+		}
+
+		public iCalendarBooking(iCalendarEvent @event, IEnumerable<BookingProtocolInfo> bookingProtocolInfo)
+		{
+			if (@event == null)
+				throw new ArgumentNullException("event");
+
+			m_Event = @event;
+
+			if (bookingProtocolInfo != null)
+				m_BookingNumbers = ParseBookingNumbers(bookingProtocolInfo).ToList();
+		}
+
+		private static IEnumerable<IDialContext> ParseBookingNumbers(IEnumerable<BookingProtocolInfo> bookingProtocolInfo)
+		{
+			foreach (BookingProtocolInfo info in bookingProtocolInfo)
+			{
+				switch (info.DialProtocol)
+				{
+					case eDialProtocol.None:
+						continue;
+
+					case eDialProtocol.Sip:
+						yield return new SipDialContext { DialString = info.Number };
+						continue;
+
+					case eDialProtocol.Pstn:
+						yield return new PstnDialContext { DialString = info.Number };
+						continue;
+
+					case eDialProtocol.Zoom:
+						yield return new ZoomDialContext { DialString = info.Number };
+						continue;
+
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+			}
+		}
+	}
+}
---
+++ ICD.Connect.Calendaring/Devices/iCalendar/Parser/iCalendarCalendar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ICD.Connect.Calendaring.Devices.iCalendar.Parser
+{
+// ReSharper disable InconsistentNaming
+	public sealed class iCalendarCalendar
+// ReSharper restore InconsistentNaming
+	{
+		private const string EVENT_REGEX = @"(?'event'BEGIN:VEVENT[\s\S]*?END:VEVENT)";
+
+		private readonly List<iCalendarEvent> m_Events;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		private iCalendarCalendar()
+		{
+			m_Events = new List<iCalendarEvent>();
+		}
+
+		/// <summary>
+		/// Gets the events in the calendar.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<iCalendarEvent> GetEvents()
+		{
+			return m_Events.ToArray();
+		}
+
+		/// <summary>
+		/// Deserializes a new calendar instance from the given iCalendar data.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static iCalendarCalendar Deserialize(string data)
+		{
+			if (data == null)
+				throw new ArgumentNullException("data");
+
+			data = data.Trim();
+			data = iCalendarUtils.StripOctetLineBreaks(data);
+
+			if (!data.StartsWith("BEGIN:VCALENDAR"))
+				throw new FormatException("Missing leading BEGIN:VCALENDAR");
+
+			if (!data.EndsWith("END:VCALENDAR"))
+				throw new FormatException("Missing trailing END:VCALENDAR");
+
+			iCalendarCalendar output = new iCalendarCalendar();
+
+			// Read the events
+			foreach (Match match in Regex.Matches(data, EVENT_REGEX))
+			{
+				iCalendarEvent iCalEvent = iCalendarEvent.Deserialize(match.Value);
+				output.m_Events.Add(iCalEvent);
+			}
+
+			return output;
+		}
+	}
+}
