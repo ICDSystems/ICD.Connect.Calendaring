86a7dc53525f04a8ad9842462a3a0bd12029e690
Austin Noska <austin.noska@profound-tech.com>
2019-09-16 17:11:41 -0400

    fix: encoding the resource ID as its escaped data string

--- ICD.Connect.Calendaring.Robin/Components/Events/EventsComponent.cs
+++ ICD.Connect.Calendaring.Robin/Components/Events/EventsComponent.cs
@@ -85,7 +85,9 @@ namespace ICD.Connect.Calendaring.Robin.Components.Events
 		/// <returns></returns>
 		private Event[] GetReservations(string resourceId, DateTime startTime, DateTime endTime)
 		{
-			string path = string.Format("spaces/{0}/events?after={1:yyyy-MM-ddTHH:mm:sszzz}&before={2:yyyy-MM-ddTHH:mm:sszzz}&per_page=300", resourceId, startTime, endTime);
+			string path =
+				string.Format("spaces/{0}/events?after={1:yyyy-MM-ddTHH:mm:sszzz}&before={2:yyyy-MM-ddTHH:mm:sszzz}&per_page=300",
+				              Uri.EscapeDataString(resourceId), startTime, endTime);
 
 			string data = Parent.Request(path);
 
