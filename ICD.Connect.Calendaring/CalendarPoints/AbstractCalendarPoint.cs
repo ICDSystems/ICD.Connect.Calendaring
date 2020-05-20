e36c5b942625a0207922c690ae64d97eb8cf1961
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-19 21:57:51 -0400

    fix: Overriding missing property, updating csproj

--- ICD.Connect.Calendaring/CalendarPoints/AbstractCalendarPoint.cs
+++ ICD.Connect.Calendaring/CalendarPoints/AbstractCalendarPoint.cs
@@ -1,9 +1,14 @@
-﻿using ICD.Connect.Devices.Points;
+﻿using ICD.Connect.Calendaring.Controls;
+using ICD.Connect.Devices.Points;
 
 namespace ICD.Connect.Calendaring.CalendarPoints
 {
-	public abstract class AbstractCalendarPoint<TSettings> : AbstractPoint<TSettings>, ICalendarPoint
+	public abstract class AbstractCalendarPoint<TSettings> : AbstractPoint<TSettings, ICalendarControl>, ICalendarPoint
 		where TSettings : ICalendarPointSettings, new()
 	{
+		/// <summary>
+		/// Gets the category for this originator type (e.g. Device, Port, etc)
+		/// </summary>
+		public override string Category { get { return "CalendarPoint"; } }
 	}
 }
