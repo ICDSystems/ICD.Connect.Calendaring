3a968847285cc321ebdbf32c63aa8dd25d215a98
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-19 22:43:47 -0400

    feat: ICalendarPoint overrides Control property

--- ICD.Connect.Calendaring/CalendarPoints/ICalendarPoint.cs
+++ ICD.Connect.Calendaring/CalendarPoints/ICalendarPoint.cs
@@ -1,8 +1,15 @@
-﻿using ICD.Connect.Devices.Points;
+﻿using ICD.Common.Properties;
+using ICD.Connect.Calendaring.Controls;
+using ICD.Connect.Devices.Points;
 
 namespace ICD.Connect.Calendaring.CalendarPoints
 {
 	public interface ICalendarPoint : IPoint
 	{
+		/// <summary>
+		/// Gets the control for this point.
+		/// </summary>
+		[CanBeNull]
+		new ICalendarControl Control { get; }
 	}
 }
