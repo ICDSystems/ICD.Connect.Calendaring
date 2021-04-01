3eff5d464ecc58144becb32dd99ceab71bb58e7b
Chris Cameron <chris.cameron@wedoresi.com>
2021-04-01 14:13:01 -0400

    feat: Added CalendarUtils method for determining if a booking takes place during a time range

---
+++ ICD.Connect.Calendaring/Utils/CalendarUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Connect.Calendaring.Bookings;
+
+namespace ICD.Connect.Calendaring.Utils
+{
+	public static class CalendarUtils
+	{
+		/// <summary>
+		/// Returns true if the given booking takes place, at least partially, between the given dates.
+		/// </summary>
+		/// <param name="booking"></param>
+		/// <param name="from"></param>
+		/// <param name="to"></param>
+		/// <returns></returns>
+		public static bool IsInRange([NotNull] IBooking booking, DateTime from, DateTime to)
+		{
+			if (booking == null)
+				throw new ArgumentNullException("booking");
+
+			return booking.StartTime <= to && from < booking.EndTime;
+		}
+	}
+}
