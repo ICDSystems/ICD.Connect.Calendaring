b8da33bf4781aeeab0a900c4d823ebce6249df2e
Drew Tingen <drew.tingen@icdsystems.com>
2021-10-05 17:25:33 -0400

    fix: Use correct Newtonsoft because Crestron

--- ICD.Connect.Calendaring/Bookings/AbstractBasicBooking.cs
+++ ICD.Connect.Calendaring/Bookings/AbstractBasicBooking.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿#if NETFRAMEWORK
+extern alias RealNewtonsoft;
+using RealNewtonsoft.Newtonsoft.Json;
+#else
+using Newtonsoft.Json;
+#endif
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using ICD.Common.Properties;
 using ICD.Common.Utils.Extensions;
 using ICD.Common.Utils.Json;
 using ICD.Connect.Conferencing.DialContexts;
-using Newtonsoft.Json;
 
 namespace ICD.Connect.Calendaring.Bookings
 {
